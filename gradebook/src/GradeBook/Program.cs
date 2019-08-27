5d1093e2f4fd47f5a2634096c9e16956f29a240f
Roderick@Job 3:14 <roderick@karlemstrand.com>
2019-08-27 11:01:05 +0200

    Add some grades and compute sum

--- gradebook/src/GradeBook/Program.cs
+++ gradebook/src/GradeBook/Program.cs
@@ -6,7 +6,18 @@ namespace GradeBook
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            if (args.Length > 0) {
+                Console.WriteLine($"Hello {args[0]}!");
+            } else {
+                Console.WriteLine("Hello stranger!");
+            }
+
+            var grades = new[] {12.7, 10.3, 6.11, 4.1};
+            var result = 0.0;
+            foreach (var grade in grades) {
+                result += grade;
+            }
+            Console.WriteLine($"The sum of all grades is {result}");
         }
     }
 }
